f8fd9f41c4a13eb8e5ae836a07ca26aa52e16a57
tinashediego <tinashediego@gmail.com>
2018-05-11 17:19:41 +0200

    validation of signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ import { InvitePage } from '../pages/invite/invite';
 import { WalletsPage } from '../pages/wallets/wallets';
 import { LogoutPage } from '../pages/logout/logout';
 import {Camera} from '@ionic-native/camera';
+import { share } from 'rxjs/operator/share';
 
 @Component({
   templateUrl: 'app.html'
