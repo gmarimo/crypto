2f29c6bd80390c722bd8c3bedd25073e7567b560
nyashaChitiyo <nyashachitiyo92@gmail.com>
2018-05-22 16:38:34 +0200

    my Btc and ethereum wallets

--- src/pages/myethwallet/myethwallet.ts
+++ src/pages/myethwallet/myethwallet.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EthdepositPage } from '../ethdeposit/ethdeposit';
 import { EthwithdrawPage } from '../ethwithdraw/ethwithdraw';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, snapshotChanges } from 'angularfire2/database';
 /**
  * Generated class for the MyethwalletPage page.
  *
@@ -17,7 +18,9 @@ import { EthwithdrawPage } from '../ethwithdraw/ethwithdraw';
 })
 export class MyethwalletPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items;
+  constructor(public fdb: AngularFireDatabase,public afAuth: AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
+    this.updatebal();
   }
 
   ionViewDidLoad() {
@@ -31,5 +34,48 @@ export class MyethwalletPage {
   withdraweth() {
     this.navCtrl.push(EthwithdrawPage);
   }
+  updatebal(){ 
+    var bal:Date;
+    var re = ".";
+    var str = this.crtUsr();
+    var newstr = str.replace(re,"");
+    this.fdb.database.ref('UserID').child(newstr).child('Ethereum').once('value', function(snapshot) {
+      if (snapshot.val() !== null) {
+        return snapshot;
+      }
+  }).then((snapshot) => {
+    let Catdata = Object.keys(snapshot.val());
+    let temparr = [];
+    let datearr: Date[]=[];
 
+    var datt:Date;
+    for (var key:number=0;key<Catdata.length;key++) {
+        //temparr.push(Catdata[key]);
+        temparr[key]=Catdata[key]
+        datearr[key] = new Date(temparr[key]);
+        datt = datearr[key]; 
+    }  
+    return this.setBal(datt);
+  });
+  }
+  crtUsr(){
+    var re = "@";
+    var str = this.afAuth.auth.currentUser.email;
+    var newstr = str.replace(re,"");
+    return newstr;
+  } 
+setBal(date:Date){
+  var re = ".";
+  var str = this.crtUsr();
+  var newstr = str.replace(re,"");
+  var bal:number;
+  var url = '/UserID/'+newstr+'/Ethereum/'+date;
+  this.fdb.list(url).valueChanges().subscribe(
+    data => {
+    this.items = data;
+    }
+  )
+  return bal;
 }
+}
+
--- src/pages/selleth/selleth.ts
+++ src/pages/selleth/selleth.ts
@@ -3,7 +3,7 @@ import { Component, ViewChild } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Buydetails } from '../../models/buydetails';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { LoadingController } from 'ionic-angular';
+import { LoadingController, ToastController} from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { WalletsPage } from '../wallets/wallets';
 //import { LoadingController } from 'ionic-angular';
@@ -38,7 +38,7 @@ export class SellethPage {
   elClass:string;
   title: string;
   
-  constructor(private dbAuth: AngularFireAuth, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams,private fdb:AngularFireDatabase) {
+  constructor(private toastCtrl:ToastController,public alertctrl: AlertController,private dbAuth: AngularFireAuth, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams,private fdb:AngularFireDatabase) {
     this.payamnt = 0;
     this.commissionRate = 0.08;
     this.getEth = 0;
@@ -81,7 +81,7 @@ export class SellethPage {
     return get;
   }
   
-  makeTransaction(){
+  makeTransaction(ethbal:number,usdBal:number){
 
     let loader = this.loadingCtrl.create({
       spinner: "bubbles",
@@ -104,6 +104,42 @@ export class SellethPage {
           GET_BTC:this.getEth,
           TOTAL:this.payamnt,
     })
+    if(this.eth < ethbal){
+      var newBal:number = usdBal + this.payamnt;
+      this.loader();
+      
+          var ethbal = ethbal - this.eth;
+          var ref1 = this.fdb.database.ref('UserID').child(newstr).child('Ethereum').child(''+date);
+          ref1.set({
+                Ethereum:ethbal,
+          })
+    
+          var ref = this.fdb.database.ref('UserID').child(newstr).child('USD Balance').child(''+date);
+          ref.set({
+                USD:newBal,
+          })
+        .catch(error => { 
+    
+            let toast = this.toastCtrl.create({
+            message: 'There is a problem completing your transaction, please try again' ,
+            duration:5000,
+            cssClass: "toastclr" 
+          });
+    
+          //////////////////////////
+          toast.present();          
+            });
+          }
+          else{
+            let toast = this.toastCtrl.create({
+              message: 'You cannot sell Ethereum that are above your float' ,
+              duration:5000,
+              cssClass: "toastclr"
+        
+            });
+            toast.present();   
+          }
+
   }
   crtUsr(){
     var re = "@";
@@ -120,4 +156,123 @@ export class SellethPage {
     this.getEth=0;
     
   }
+
+  ////// get Usd balance
+  getBal(){
+    var bal:Date;
+    var re = ".";
+    var str = this.crtUsr();
+    var newstr = str.replace(re,"");
+    this.fdb.database.ref('UserID').child(newstr).child('USD Balance').once('value', function(snapshot) {
+      if (snapshot.val() !== null) {
+      }
+  }).then((snapshot) => {
+    let Catdata = Object.keys(snapshot.val());
+    let temparr = [];
+    let datearr: Date[]=[];
+
+    var datt:Date;
+    for (var key:number=0;key<Catdata.length;key++) {
+        //temparr.push(Catdata[key]);
+        temparr[key]=Catdata[key]
+        datearr[key] = new Date(temparr[key]);
+        datt = datearr[key]; 
+    }  
+    return this.getCurrentUsdBal(datt);
+  });
+  }
+
+  getCurrentUsdBal(date:Date){
+    var re = ".";
+    var str = this.crtUsr();
+    var newstr = str.replace(re,"");
+    var usdbal:number;
+    var url = '/UserID/'+newstr+'/USD Balance/'+date;
+    this.fdb.list(url).valueChanges().subscribe(
+      data => {
+      var strbal:string = data.toString();
+      usdbal = +strbal
+      this.getEthBal(usdbal);
+      }
+    )
+    return usdbal;
+  }
+
+
+  getEthBal(usdBal:number){
+    var re = ".";
+    var str = this.crtUsr();
+    var newstr = str.replace(re,"");
+    this.fdb.database.ref('UserID').child(newstr).child('Ethereum').once('value', function(snapshot) {
+      if (snapshot.val() !== null) {
+      }
+  }).then((snapshot) => {
+    let Catdata = Object.keys(snapshot.val());
+    let temparr = [];
+    let datearr: Date[]=[];
+  
+    var datt:Date;
+    for (var key:number=0;key<Catdata.length;key++) {
+        temparr[key]=Catdata[key]
+        datearr[key] = new Date(temparr[key]);
+        datt = datearr[key]; 
+    }  
+    
+    return this.getCurrentEthBal(datt,usdBal);
+  });
+  }
+  getCurrentEthBal(date:Date,usdBal){
+    var re = ".";
+    var str = this.crtUsr();
+    var newstr = str.replace(re,"");
+    var ethbal:number;
+    var url = '/UserID/'+newstr+'/Ethereum/'+date;
+    this.fdb.list(url).valueChanges().subscribe(
+      data => {
+      var strbal:string = data.toString();
+      ethbal = +strbal
+      this.makeTransaction(ethbal,usdBal);
+      }
+    )
+  }
+  public loader(){
+    if(this.usdamnt.value!=''&& this.ethamnt.value!=''){
+      let loader = this.loadingCtrl.create({
+ 
+        spinner:"bubbles",
+        content:"Completing your transaction ..",
+        duration:5000 
+      }); 
+      loader.onDidDismiss(() => {
+       //console.log('Dismissed loading');
+        let alert = this.alertctrl.create({  
+          title: "Transaction Status",
+          subTitle: "Completed successfully!",
+          buttons: [
+
+            {
+              text: 'ok',
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked');
+              }
+            },
+            {
+              text: 'View History',
+              handler: () => {
+                //console.log('Buy clicked');
+                this.navCtrl.push(BtcbuysuccessPage);
+              }
+            }
+
+
+
+          ] 
+        });
+        alert.present();       
+     });  
+     loader.present()
+  }
+ }
+
 }
